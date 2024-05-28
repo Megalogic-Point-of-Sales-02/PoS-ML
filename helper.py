f432f707ba00b1eff80226246167d2b5f3599409
shafaleyda <shafaleyda@gmail.com>
2024-05-29 00:52:58 +0700

    feat: deploy churn prediction model

---
+++ helper.py
@@ -0,0 +1,10 @@
+# import numpy as np
+# from sklearn.preprocessing import MinMaxScaler
+
+# def scale_data(col: np.ndarray, col_name: str):
+    
+#     min = 
+#     max = 
+#     scaler = MinMaxScaler(feature_range=(0, 1)) 
+#     scaled_data = scaler.fit_transform(col)
+#     return scaled_data
